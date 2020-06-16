d3223869f2c38650b11ed66721d639ef5806fb31
rodriguezh21 <rodriguezh21@gmail.com>
2020-06-15 22:08:33 -0500

    fixed saveRecord error

--- public/db.js
+++ public/db.js
@@ -1,18 +1,19 @@
-const db;
+let db;
 const request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
-    db.event.target.result;
+    db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 }
 
 request.onsuccess = event => {
-    console.log(request.result)
-}
-// check if app is online before reading from db
-if (navigator.onLine) {
+  db = event.target.result
+  
+  // check if app is online before reading from db
+  if (navigator.onLine) {
     checkDatabase();
   }
+}
 
 // Referenced from week 17 activity 26
 
